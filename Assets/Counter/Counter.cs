4c7abe37dd63525e09defd18bbf50673eb1d0b0e
ManelNeon <neonextra0@gmail.com>
2024-03-02 17:35:30 +0000

    Last Commit
    Commit with everything done, and with the design document in it too.

--- Assets/Counter/Counter.cs
+++ Assets/Counter/Counter.cs
@@ -6,18 +6,40 @@ using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
+    //Counter text in the UI
     public Text CounterText;
 
-    private int Count = 0;
+    //Counter
+    [HideInInspector] public int count = 0;
+
+    //Manger to get the time
+    GameManager manager;
+
+    //sound in unity inspector
+    [SerializeField] AudioClip gotBall;
+
+    //audio source
+    AudioSource playerAudio;
 
     private void Start()
     {
-        Count = 0;
+        //getting audio source component
+        playerAudio = GetComponent<AudioSource>();
+        //establishing the manager
+        manager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        count = 0;
     }
-
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        Count += 1;
-        CounterText.text = "Count : " + Count;
+        //playing the gotball sound
+        playerAudio.PlayOneShot(gotBall, 1.0f);
+        //setting the ball false
+        collision.gameObject.SetActive(false);
+        //getting more time when getting a ball
+        manager.time += 5;
+        //getting one more on the count
+        count += 1;
+        //changing the text in the UI
+        CounterText.text = "Count : " + count;
     }
 }
